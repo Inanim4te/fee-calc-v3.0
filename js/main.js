73d326a02af84090dda5bd031944386f28c36dd3
Inanim4te <Obsidianuz689@gmail.com>
2023-05-24 14:15:23 +0300

    percentages change

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ function func() {
     case '1%':
       result = Math.floor(num1 - num1 * 0.01);
       break;
-    case '2.5%':
-      result = Math.floor(num1 - num1 * 0.025);
+    case '1.5%':
+      result = Math.floor(num1 - num1 * 0.015);
       break;
     case '-50':
       result = Math.floor(num1 - 50);
